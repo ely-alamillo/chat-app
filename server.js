b2ee382130956d10d1397e9c2f07811d35fa510b
Ely Alamillo <ely0013@gmail.com>
2017-09-27 21:10:28 -0500

    completed socket functionality

--- server.js
+++ server.js
@@ -16,15 +16,29 @@ io.on('connection', function(socket) {
 
   // DISCONNECT
   socket.on('disconnect', function(data) {
+    // if (!socket.username) return
+    users.splice(users.indexOf(socket.username), 1);
+    updateUsernames();
     connections.splice(connections.indexOf(socket), 1);
     console.log(`Disconnected: ${connections.length} sockets connected`);
 
   });
 
   socket.on('send message', function(data) {
-    io.sockets.emit('new message', { msg: data });
+    io.sockets.emit('new message', { msg: data, user: socket.username });
   });
 
+  // new user
+  socket.on('new user', function(data, cb) {
+    cb(true);
+    socket.username = data;
+    users.push(socket.username);
+    updateUsernames();
+  });
+
+  function updateUsernames() {
+    io.sockets.emit('get users', users)
+  };
 });
 
 server.listen(3000, () => {
